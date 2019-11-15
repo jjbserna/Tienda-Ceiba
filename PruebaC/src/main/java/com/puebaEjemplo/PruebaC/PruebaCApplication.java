17da592d3058c9161d2d0df43d0d45b4e2d31532
Jeison Julían Barbosa Serna - Ceiba Software <jeison.barbosa@ceiba.com.co>
2019-11-15 07:39:35 -0500

    git inicial para prueba Jenkins

---
+++ PruebaC/src/main/java/com/puebaEjemplo/PruebaC/PruebaCApplication.java
@@ -0,0 +1,13 @@
+package com.puebaEjemplo.PruebaC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaCApplication.class, args);
+	}
+
+}
---
+++ PruebaC/src/test/java/com/puebaEjemplo/PruebaC/PruebaCApplicationTests.java
@@ -0,0 +1,13 @@
+package com.puebaEjemplo.PruebaC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaCApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
