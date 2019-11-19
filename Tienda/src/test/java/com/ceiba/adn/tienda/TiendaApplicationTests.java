84c55210481c67a3e66a4873ea1aaf8405f77221
Jeison Julían Barbosa Serna - Ceiba Software <jeison.barbosa@ceiba.com.co>
2019-11-18 21:56:05 -0500

    Revisión  pruebas fallidas Jenkins, verificación de modulos

--- Tienda/src/test/java/com/ceiba/adn/tienda/TiendaApplicationTests.java
+++ Tienda/src/test/java/com/ceiba/adn/tienda/TiendaApplicationTests.java
@@ -1,9 +1,8 @@
 package com.ceiba.adn.tienda;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class TiendaApplicationTests {
 
 	@Test
--- Tienda/src/test/java/com/ceiba/adn/tienda/dominio/unitarias/PrendaTest.java
+++ Tienda/src/test/java/com/ceiba/adn/tienda/dominio/unitarias/PrendaTest.java
@@ -14,20 +14,20 @@ import com.ceiba.adn.tienda.testdatabuilder.PrendaTestDataBuilder;
  * @author jeison.barbosa
  *
  */
-public class PredaTest {
+public class PrendaTest {
 
 	private static final String DESCRIPCION = "Camisa DevOps";
 	private static final String ESTILO = "DevOps";
 	private static final boolean ESTADO = true;
 	private static final double PRECIO = 100000;
 	private static final int STOCK = 100;
-	
-	
-@Test
+
+	@Test
 	public void crearPrendaTest() {
 
 		// arrange
-		PrendaTestDataBuilder prendaTestDataBuilder = new PrendaTestDataBuilder().conDescripcion(DESCRIPCION).conEstilo(ESTILO).conEstado(ESTADO).conPrecio(PRECIO).conStock(STOCK);
+		PrendaTestDataBuilder prendaTestDataBuilder = new PrendaTestDataBuilder().conDescripcion(DESCRIPCION)
+				.conEstilo(ESTILO).conEstado(ESTADO).conPrecio(PRECIO).conStock(STOCK);
 
 		// act
 		Prenda prenda = prendaTestDataBuilder.build();
